1379177e5de473b2e795745660368e5e18524fe4
Raghu Betina <rbetina@gmail.com>
2013-04-29 16:51:52 -0500

    Added Movie#director method and cleaned up views with it.

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,5 @@
 class Director < ActiveRecord::Base
+  def movies
+
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  def director
+    return Director.find_by_id(self.director_id)
+  end
 end
