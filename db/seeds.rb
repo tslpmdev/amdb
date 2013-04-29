ca67479367ad3322fbd04f7d3850768584227b40
Raghu Betina <rbetina@gmail.com>
2013-04-29 10:45:12 -0500

    Added directors to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+director_hashes = [
+  { :name => "Christopher Nolan", :dob => "30/07/1970" },
+  { :name => "Francis Ford Coppola", :dob => "07/04/1939" },
+  { :name => "Frank Darabont", :dob => "29/01/1959" }
+]
+
+Director.destroy_all
+director_hashes.each do |d_hash|
+  d = Director.new
+  d.name = d_hash[:name]
+  d.dob = d_hash[:dob]
+  d.save
+end
+puts "There are now #{Director.count} rows in the directors table."
