489838cd3cbe0c010e5ea8795a2059b3c2ae7aff
Raghu Betina <rbetina@gmail.com>
2013-04-29 10:41:08 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amdb::Application.config.secret_token = '55e83017aea7aa58c02f828148bb2f04534e2f4b7ade53900b2a5e8e267bd5cb10aa072adf2534c6bf9d54f1210068e9924904d8eff1c3de55d8f4c41cf2ec02'
