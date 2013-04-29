e72449a8e5aa441729231d842e977a5450237c01
Raghu Betina <rbetina@gmail.com>
2013-04-29 10:51:51 -0500

    Added movies to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,21 @@ director_hashes.each do |d_hash|
   d.save
 end
 puts "There are now #{Director.count} rows in the directors table."
+
+
+movie_hashes = [
+  { :title => "The Dark Knight", :year => 2008, :director_id => Director.find_by_name("Christopher Nolan").id },
+  { :title => "The Godfather", :year => 1972, :director_id => Director.find_by_name("Francis Ford Coppola").id },
+  { :title => "The Shawshank Redemption", :year => 1994, :director_id => Director.find_by_name("Frank Darabont").id },
+  { :title => "The Godfather: Part II", :year => 1974, :director_id => Director.find_by_name("Francis Ford Coppola").id }
+]
+
+Movie.destroy_all
+movie_hashes.each do |m_hash|
+  m = Movie.new
+  m.title = m_hash[:title]
+  m.year = m_hash[:year]
+  m.director_id = m_hash[:director_id]
+  m.save
+end
+puts "There are now #{Movie.count} rows in the movies table."
