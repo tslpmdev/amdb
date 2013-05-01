5c01f4d92c0f392d6583d54e7707a1f020fc0940
Raghu Betina <rbetina@gmail.com>
2013-05-01 14:44:57 -0500

    Stubbed out Role#actor and Role#movie methods.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,9 @@
 class Role < ActiveRecord::Base
+  def actor
+    # This method should return the actor that this role belongs to
+  end
+
+  def movie
+    # This method should return the movie that this role belongs to
+  end
 end
