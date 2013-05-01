e978754414c24ae105152a61b1935f46fe769fe9
Raghu Betina <rbetina@gmail.com>
2013-05-01 13:06:07 -0500

    Reset a few things ahead of Week 5 Day 2.

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,5 @@
 class Director < ActiveRecord::Base
   def movies
-
+    # This method should return an array of all the movies that belong to this director
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ActiveRecord::Base
   def director
+    # This method should return the single director that this movie belongs to
     return Director.find_by_id(self.director_id)
   end
 end
