b9cb06a0d8599c1bc3136bd988ad817c4759be9d
Raghu Betina <rbetina@gmail.com>
2013-05-01 15:58:25 -0500

    Cleaned up roles index page. Stubbed out Actor#roles and Movie#roles methods.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,5 @@
 class Actor < ActiveRecord::Base
+  def roles
+    # This method should return an array of all the roles that belong to this actor
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ActiveRecord::Base
     # This method should return the single director that this movie belongs to
     return Director.find_by_id(self.director_id)
   end
+
+  def roles
+    # This method should return an array of all the roles that belong to this movie
+  end
 end
