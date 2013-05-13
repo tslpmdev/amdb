35c66e9836171ec9d3e0d2d350b3b72d0c75cab0
Raghu Betina <rbetina@gmail.com>
2013-05-13 14:42:09 -0500

    Dried up authorization logic into before_filter.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
 
+  before_filter :find_user, except: [:new, :create, :index]
+  before_filter :authorize_user, only: [:show, :update, :destroy, :edit]
+
+  def find_user
+    @user = User.find_by_id(params[:id])
+  end
+
+  def authorize_user
+    if @user.id != session[:user_id]
+      redirect_to users_url, notice: "Nice try."
+    end
+  end
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find_by_id(params[:id])
-
-    if session[:user_id] != @user.id
-      redirect_to users_url, notice: "Nice try."
-    end
   end
 
   def new
@@ -31,40 +39,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by_id(params[:id])
-
-    if @user.id != session[:user_id]
-      redirect_to users_url, notice: "Nice try."
-    end
   end
 
   def update
-    @user = User.find_by_id(params[:id])
+    @user.username = params[:username]
+    @user.password = params[:password]
+    @user.password_confirmation = params[:password_confirmation]
 
-    if @user.id == session[:user_id]
-      @user.username = params[:username]
-      @user.password = params[:password]
-      @user.password_confirmation = params[:password_confirmation]
-
-      if @user.save
-        redirect_to users_url, :notice => "Updated successfully."
-      else
-        redirect_to edit_user_url(@user.id), :notice => "Username taken."
-      end
+    if @user.save
+      redirect_to users_url, :notice => "Updated successfully."
     else
-      redirect_to users_url, notice: "Nice try."
+      redirect_to edit_user_url(@user.id), :notice => "Username taken."
     end
   end
 
   def destroy
-    @user = User.find_by_id(params[:id])
-    if session[:user_id] == @user.id
-      @user.destroy
-      reset_session
-      redirect_to users_url
-    else
-      redirect_to users_url, notice: "Nice try."
-    end
+    @user.destroy
+    reset_session
+    redirect_to users_url
   end
 end
 
