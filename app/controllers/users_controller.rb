cd9f7b0322ca737fc0ed4e5ee5f420c04ca3b0a5
Raghu Betina <rbetina@gmail.com>
2013-05-13 14:20:50 -0500

    Finished authorization in users controller the not-DRY way.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+
+    if session[:user_id] != @user.id
+      redirect_to users_url, notice: "Nice try."
+    end
   end
 
   def new
@@ -19,6 +23,7 @@ class UsersController < ApplicationController
     @user.password_confirmation = params[:password_confirmation]
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to users_url, :notice => "Created successfully."
     else
       redirect_to new_user_url, :notice => "Username taken."
@@ -27,24 +32,50 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find_by_id(params[:id])
+
+    if @user.id != session[:user_id]
+      redirect_to users_url, notice: "Nice try."
+    end
   end
 
   def update
     @user = User.find_by_id(params[:id])
-    @user.username = params[:username]
-    @user.password = params[:password]
-    @user.password_confirmation = params[:password_confirmation]
 
-    if @user.save
-      redirect_to users_url, :notice => "Updated successfully."
+    if @user.id == session[:user_id]
+      @user.username = params[:username]
+      @user.password = params[:password]
+      @user.password_confirmation = params[:password_confirmation]
+
+      if @user.save
+        redirect_to users_url, :notice => "Updated successfully."
+      else
+        redirect_to edit_user_url(@user.id), :notice => "Username taken."
+      end
     else
-      redirect_to edit_user_url(@user.id), :notice => "Username taken."
+      redirect_to users_url, notice: "Nice try."
     end
   end
 
   def destroy
     @user = User.find_by_id(params[:id])
-    @user.destroy
-        redirect_to users_url
-      end
+    if session[:user_id] == @user.id
+      @user.destroy
+      reset_session
+      redirect_to users_url
+    else
+      redirect_to users_url, notice: "Nice try."
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
