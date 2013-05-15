6e7670198d376485cedf83d5ca1de83a0fb36a4e
Raghu Betina <rbetina@gmail.com>
2013-05-15 15:27:07 -0500

    Stubbed out custom validation method.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,4 +6,24 @@ class Vote < ActiveRecord::Base
   validates :movie, presence: true
   # validates :user_id, uniqueness: true
   # add a custom validation method that will prevent a user from voting more than three times
+
+  validate :user_cannot_vote_more_than_three_times
+
+  def user_cannot_vote_more_than_three_times
+    if # you write your rule
+      errors.add(:user_id, "has already voted three times")
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
